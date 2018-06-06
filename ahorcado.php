be8cc2ec9b04c79afbf3e35bf16761d8b564c1ba
SebastianCabreraZamora <sebastiancz17@gmail.com>
2018-06-06 10:39:08 -0800

    Guardo lo que teniamos antes en la carpeta "PruebaAnterior" + Agrego lo que estaba en el drive en la carpeta "juegoAhorcado" + Le agrego comentarios

---
+++ ahorcado.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+    <head>        
+        <title>Juego Ahorcado</title>
+    </head>
+    <body bgcolor="lightblue">
+        <form action="" method="post" name="formletra" id="formletra">
+            <p align="center">letra: <input type="text" name="letra" id="letra" autofocus></p>
+          
+            <p align="center"><input type="submit" name="Verificar" id="Verificar" value="Verificar"></p>
+        </form>
+    </body>
+</html>
+
+<?php
+	//No se pueden usar variables globales en vez de usar $_SESSION?
+	//Se podrá acomodar en forma de funciones para que sea más manejable?
+	//Voy a ver si se puede llamar a este archivo 'index' y que comience desde aquí
+	//	|->La parte de un menú se podría agregar en Visual
+    session_start();    
+    
+    if ($_SESSION['primeravez']){
+        //Numero de intentos de usuario
+        $_SESSION['intentos'] = -1;
+		//(IGNORAR)Esto es por si se quiere jugar con una palabra que uno ingrese
+		//$_SESSION['palabraingresada'] = $_POST['palabra'];  
+		//El .txt con las palabras
+        $contenido = file("palabras.txt");
+        $linea_azar = $contenido[rand(0, count($contenido) - 1)];
+		$linea_azar = trim($linea_azar);
+		//Palabra dividida en letras escogida al azar de $contenido
+       	$_SESSION['palabraingresada']= $linea_azar;
+		//No mostrar la respuesta a menos que se esten haciendo pruebas
+        //echo $_SESSION['palabraingresada'];
+		//Longitud de la palabra elegida
+        $_SESSION['longitud'] = strlen($_SESSION['palabraingresada']);
+		     
+        //Letras que se pueden usar como intento valido
+        $_SESSION['permitidos'] = " -abcdefghijklmnopqrstuvwxyz";
+        
+		//Hilera de '_' del tamano de la palabra escogida
+        for ($i = 0; $i < $_SESSION['longitud']; $i++){
+            $_SESSION['ahorcado'][$i] = '_';
+        }
+        $_POST['letra'] = ' ';
+        $_SESSION['primeravez'] = false;
+    }
+
+    $_SESSION['letra'] = $_POST['letra'];
+    
+	//Se verifica si se ingreso una letra como intento
+    $letraVacia = false;
+    if ($_SESSION['letra'] == ''){
+        $letraVacia = true;
+        $_SESSION['letra'] = '-';
+        echo '<script type = "text/javascript">
+            alert("DEBE INGRESAR UNA LETRA");            
+        </script>';
+    }
+    
+	//Se verifica si la letra es permitida
+    $indicadorLetraPermitida = true;
+    if (stripos($_SESSION['permitidos'], $_SESSION['letra']) === false){
+        echo '<script type = "text/javascript">
+            alert("La letra no es valida");            
+        </script>';
+        $indicadorLetraPermitida = false;
+    }
+    
+	//Se guardan todas las letras ya utilizadas para eviatr que sean repetidas
+    if ($indicadorLetraPermitida == true && $_SESSION['letra'] != '-'){
+        $_SESSION['LetraIngresada'][] = $_SESSION['letra'];
+    }
+    
+	//Si la letra esta en la palabra, se sustituye en la hilera de '_'
+    $indicadorIntentos = false;
+    for ($i = 0; $i < $_SESSION['longitud']; $i++){
+        if ($_SESSION['palabraingresada'][$i] == $_SESSION['letra']){
+            $_SESSION['ahorcado'][$i] = $_SESSION['letra'];
+            $indicadorIntentos = true;
+        }
+    }
+    
+	//Se verifica que la letra no haya sido ingresada antes
+    $contadorLetras = 0;   
+    $indicadorLetraIn = false;
+    if ($_SESSION['letra'] != ' '){
+        for ($i=0; $i<count($_SESSION['LetraIngresada']); $i++){
+            if ($_SESSION['LetraIngresada'][$i] == $_SESSION['letra']){            
+//                $i = count($_SESSION['LetraIngresada']) + 1;
+                $contadorLetras ++;                
+            }
+        }        
+        if ($contadorLetras >= 2){
+            echo '<script type = "text/javascript">
+            alert("La letra ya ha sido ingresada");            
+        </script>';
+            $indicadorLetraIn = true;
+        }
+    }
+    
+	//Solo se cuenta como un intento si cumple con todas las reglas
+    if ($indicadorIntentos == false && $indicadorLetraPermitida == true && $indicadorLetraIn == false && $letraVacia == false){
+        $_SESSION['intentos'] ++;
+    }
+    
+	//Verifica si ya se gano el juego
+    $aux = 0;
+    for ($j=0; $j<$_SESSION['longitud']; $j++){
+        if ($_SESSION['palabraingresada'][$j] == $_SESSION['ahorcado'][$j]){
+            $aux++;
+        }
+    }    
+    if ($aux == $_SESSION['longitud']){
+        echo '<br>';
+        ?>
+        <html>
+            <p align="center">
+			<!--Si se gana el juego se imprime la palabra encontrada-->
+                <?php 
+                    for ($k=0; $k<$_SESSION['longitud']; $k++){        
+                        echo $_SESSION['ahorcado'][$k] . ' ';
+                    }
+                ?>
+            </p>
+        </html>
+        <?php
+        echo '<script type = "text/javascript">
+            alert("FELICIDADES!! HA GANADO!!");
+        </script>';
+        mensajeJugarNuevamente();
+    }
+    
+	//Se verifica si ha perdido el juego
+    if ($_SESSION['intentos'] == 9){
+        echo '<img src="imagenes/9.png">';
+        sleep(0.2);
+        echo '<br>';
+        ?>
+        <html>
+            <p align="center">
+                <?php 
+                    for ($k=0; $k<$_SESSION['longitud']; $k++){        
+                        echo $_SESSION['palabraingresada'][$k] . ' ';
+                    }
+                ?>
+            </p>
+        </html>
+<?php
+    echo '<script type = "text/javascript">
+        alert("HA PERDIDO");;
+    </script>';
+    mensajeJugarNuevamente();
+    }else{
+        echo '<p align="center">Lleva ' . $_SESSION['intentos'] . '  intentos fallidos</p>';
+    }
+?>
+<html>
+    <p align="center">
+        <?php 
+            for ($k=0; $k<$_SESSION['longitud']; $k++){        
+                echo $_SESSION['ahorcado'][$k] . ' ';
+            }
+        ?>
+    </p>
+</html>    
+<?php
+	//La imagen que se muestra dependiendo del numero de intentos
+    switch ($_SESSION['intentos']){
+        case 0:
+            echo '<p align="center"><img src="imagenes/0.png"></p>';
+            break;
+        case 1:
+            echo '<p align="center"><img src="imagenes/1.png"></p>';
+            break;
+        case 2:
+            echo '<p align="center"><img src="imagenes/2.png"></p>';
+            break;
+        case 3:
+            echo '<p align="center"><img src="imagenes/3.png"></p>';
+            break;
+        case 4:
+            echo '<p align="center"><img src="imagenes/4.png"></p>';
+            break;
+        case 5:
+            echo '<p align="center"><img src="imagenes/5.png"></p>';
+            break;
+        case 6:
+            echo '<p align="center"><img src="imagenes/6.png"></p>';
+            break;
+        case 7:
+            echo '<p align="center"><img src="imagenes/7.png"></p>';
+            break;
+        case 8:
+            echo '<p align="center"><img src="imagenes/8.png"></p>';
+            break;
+//        case 9:
+//            echo '<img src="imagenes/9.png">';
+//            break;
+        default:
+            echo '';
+    }
+    function mensajeJugarNuevamente(){
+        session_destroy();
+        echo '<script type="text/javascript">            
+            if (confirm("DESEA JUGAR DE NUEVO?") == true) {
+                window.location = "index.php";
+            }else{
+                window.location = "http://www.google.com";
+            }
+        </script>';
+    }  
+?>
--- index.php
+++ index.php
@@ -1,86 +1,23 @@
-<?php session_start();?>
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>The Hangman</title>
-	</head>
-	
-	<body>
-	
-		<?php
-			include 'funciones.php';
-			//Hay que hacerlo sin usar los $_SESSION
-			
-			if(isset($POST['userInput'])) //Si el usuario ha hecho un intento
-				{
-					$userInput = $_POST['userInput']; //Se asigna el intetnto del usuario a $userInput
-					//Se revisa si el valor ingresado es correcto
-					$hidden = checkAndReplace(strtolower($userInput), $hidden, $answer);
-					//$hidden = checkAndReplace(strtolower($userInput), $_SESSION['hidden'], $_SESSION['answer']);
-					checkGameOver($MAX_ATTEMPS, $_SESSION['attempts'], $_SESSION['answer'], $_SESSION['hidden']);
-				}
-			$_SESSION['attempts'] = $_SESSION['attempts'] +1;
-			echo 'Attempts remaining: '.($MAX_ATTEMPS - $_SESSION['attempts']).'<br>';
-		?>
-	
-	
-	
-		<?php
-			if(isset($_SESSION['newWord'])) //Se pide una nueva palabra
-			{
-				unset($_SESSION['answer']);
-			}
-			if(!isset($_SESSION['answer'])) //Se esta comenzando el juego
-			{
-				$_SESSION['attempts']=0;
-				$answer = fetchWordArray($WORLDLISTFILE); //Guarda la palabra dividida en letras
-				$_SESSION['answer'] = $answer;
-				$_SESSION['hidden'] = hide_characters($answer); //Es el string con los espacios en blanco ----
-				echo 'Attempts remaining: '.($MAX_ATTEMPS-$_SESSION['attempts']).'<br>';
-			}
-			else
-			{
-				if(isset($POST['userInput'])) //Si el usuario ha hecho un intento
-				{
-					$userInput = $_POST['userInput'];
-					//Se revisa si el valor ingresado es correcto
-					$_SESSION['hidden'] = checkAndReplace(strtolower($userInput), $_SESSION['hidden'], $_SESSION['answer']);
-					checkGameOver($MAX_ATTEMPS, $_SESSION['attempts'], $_SESSION['answer'], $_SESSION['hidden']);
-				}
-				$_SESSION['attempts'] = $_SESSION['attempts'] +1;
-				echo 'Attempts remaining: '.($MAX_ATTEMPS-$_SESSION['attempts']).'<br>';
-			}
-			echo '<br>';
-			$hidden = $_SESSION['hidden'];
-			
-			foreach($hidden as $char)
-			{
-				echo $char." ";
-			}
-		?>
-		
-		<script type="application/javascript">
-			function validateInput()
-			{
-				var x = document.forms["inputForm"]["userInput"].value;
-				if(x=="" || x==" ")
-				{
-					alert("Please entre a character");
-					return false;
-				}
-				if(!isNan(x))
-				{
-					alert("Please entre a character");
-					return false;
-				}
-			}
-		</script>
-		
-		<form name="inputForm" action="" method="post">
-			Your Guess: <input name="userInput" type="text" size="1" maxlength="1">
-			<input type="submit" value="Check" onClick="return validateInput();">
-			<input type="submit" value="Try Another Word" name="newWord" value="submit">
-		</form>
-		
-	</body>
-	
+    <head>        
+        <title>Juego Ahorcado</title>
+    </head>
+    <body bgcolor="lightblue">
+        <form action="ahorcado.php" method="post" name="formulario" id="formulario">
+            <br><br>
+            <h3 align="center"> AHORCADO</h3>
+            
+            <p align="center"><img src="imagenes/9.png"></p>
+            <br>
+			<!--Esto es por si se quiere jugar con una palabra que uno ingrese-->
+            <!--<p align="center">palabra: <input type="password" name="palabra" id="palabra" autofocus></p> -->
+            
+            <p align="center"><input type="submit" name="enviar" id="enviar" value="Jugar"></p>
+        </form>
+    </body>
 </html>
+<?php
+    session_start();
+    $_SESSION['primeravez'] = true;
+?>
