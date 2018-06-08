28b35e3715d12501a2dcc5a7a572dbea98310be2
SebastianCabreraZamora <sebastiancz17@gmail.com>
2018-06-07 18:44:44 -0800

    Agrego el Visual

---
+++ AhorcadoVisual/JuegoAhorcado/JuegoAhorcado/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace JuegoAhorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelEscondida = new System.Windows.Forms.Panel();
+            this.buttonVerificar = new System.Windows.Forms.Button();
+            this.labelEscondida = new System.Windows.Forms.Label();
+            this.panelImagen = new System.Windows.Forms.Panel();
+            this.textBoxLetra = new System.Windows.Forms.TextBox();
+            this.labelMensaje = new System.Windows.Forms.Label();
+            this.labelIntentos = new System.Windows.Forms.Label();
+            this.panelEscondida.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelEscondida
+            // 
+            this.panelEscondida.Controls.Add(this.labelEscondida);
+            this.panelEscondida.Location = new System.Drawing.Point(215, 101);
+            this.panelEscondida.Name = "panelEscondida";
+            this.panelEscondida.Size = new System.Drawing.Size(200, 100);
+            this.panelEscondida.TabIndex = 0;
+            // 
+            // buttonVerificar
+            // 
+            this.buttonVerificar.Location = new System.Drawing.Point(299, 344);
+            this.buttonVerificar.Name = "buttonVerificar";
+            this.buttonVerificar.Size = new System.Drawing.Size(102, 37);
+            this.buttonVerificar.TabIndex = 1;
+            this.buttonVerificar.Text = "Verificar";
+            this.buttonVerificar.UseVisualStyleBackColor = true;
+            this.buttonVerificar.Click += new System.EventHandler(this.buttonVerificar_Click);
+            // 
+            // labelEscondida
+            // 
+            this.labelEscondida.AutoSize = true;
+            this.labelEscondida.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelEscondida.Location = new System.Drawing.Point(0, 0);
+            this.labelEscondida.Name = "labelEscondida";
+            this.labelEscondida.Size = new System.Drawing.Size(82, 20);
+            this.labelEscondida.TabIndex = 0;
+            this.labelEscondida.Text = "escondida";
+            // 
+            // panelImagen
+            // 
+            this.panelImagen.Location = new System.Drawing.Point(545, 154);
+            this.panelImagen.Name = "panelImagen";
+            this.panelImagen.Size = new System.Drawing.Size(71, 100);
+            this.panelImagen.TabIndex = 2;
+            // 
+            // textBoxLetra
+            // 
+            this.textBoxLetra.Location = new System.Drawing.Point(299, 264);
+            this.textBoxLetra.Name = "textBoxLetra";
+            this.textBoxLetra.Size = new System.Drawing.Size(100, 26);
+            this.textBoxLetra.TabIndex = 4;
+            // 
+            // labelMensaje
+            // 
+            this.labelMensaje.AutoSize = true;
+            this.labelMensaje.Location = new System.Drawing.Point(327, 412);
+            this.labelMensaje.Name = "labelMensaje";
+            this.labelMensaje.Size = new System.Drawing.Size(51, 20);
+            this.labelMensaje.TabIndex = 5;
+            this.labelMensaje.Text = "label1";
+            // 
+            // labelIntentos
+            // 
+            this.labelIntentos.AutoSize = true;
+            this.labelIntentos.Location = new System.Drawing.Point(545, 409);
+            this.labelIntentos.Name = "labelIntentos";
+            this.labelIntentos.Size = new System.Drawing.Size(51, 20);
+            this.labelIntentos.TabIndex = 6;
+            this.labelIntentos.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 441);
+            this.Controls.Add(this.labelIntentos);
+            this.Controls.Add(this.labelMensaje);
+            this.Controls.Add(this.textBoxLetra);
+            this.Controls.Add(this.panelImagen);
+            this.Controls.Add(this.buttonVerificar);
+            this.Controls.Add(this.panelEscondida);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelEscondida.ResumeLayout(false);
+            this.panelEscondida.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelEscondida;
+        private System.Windows.Forms.Label labelEscondida;
+        private System.Windows.Forms.Button buttonVerificar;
+        private System.Windows.Forms.Panel panelImagen;
+        private System.Windows.Forms.TextBox textBoxLetra;
+        private System.Windows.Forms.Label labelMensaje;
+        private System.Windows.Forms.Label labelIntentos;
+    }
+}
+
---
+++ JuegoAhorcado/JuegoAhorcado/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace JuegoAhorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPalabraEscondida = new System.Windows.Forms.Panel();
+            this.panelImagen = new System.Windows.Forms.Panel();
+            this.labelPalabraEscondida = new System.Windows.Forms.Label();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.panelPalabraEscondida.SuspendLayout();
+            this.panelImagen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelPalabraEscondida
+            // 
+            this.panelPalabraEscondida.Controls.Add(this.labelPalabraEscondida);
+            this.panelPalabraEscondida.Location = new System.Drawing.Point(383, 153);
+            this.panelPalabraEscondida.Name = "panelPalabraEscondida";
+            this.panelPalabraEscondida.Size = new System.Drawing.Size(200, 100);
+            this.panelPalabraEscondida.TabIndex = 0;
+            // 
+            // panelImagen
+            // 
+            this.panelImagen.Controls.Add(this.labelImage);
+            this.panelImagen.Location = new System.Drawing.Point(383, 438);
+            this.panelImagen.Name = "panelImagen";
+            this.panelImagen.Size = new System.Drawing.Size(200, 100);
+            this.panelImagen.TabIndex = 1;
+            // 
+            // labelPalabraEscondida
+            // 
+            this.labelPalabraEscondida.AutoSize = true;
+            this.labelPalabraEscondida.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelPalabraEscondida.Location = new System.Drawing.Point(0, 0);
+            this.labelPalabraEscondida.Name = "labelPalabraEscondida";
+            this.labelPalabraEscondida.Size = new System.Drawing.Size(140, 20);
+            this.labelPalabraEscondida.TabIndex = 0;
+            this.labelPalabraEscondida.Text = "Palabra escondida";
+            // 
+            // labelImage
+            // 
+            this.labelImage.AutoSize = true;
+            this.labelImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelImage.Location = new System.Drawing.Point(0, 0);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(51, 20);
+            this.labelImage.TabIndex = 0;
+            this.labelImage.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 670);
+            this.Controls.Add(this.panelImagen);
+            this.Controls.Add(this.panelPalabraEscondida);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelPalabraEscondida.ResumeLayout(false);
+            this.panelPalabraEscondida.PerformLayout();
+            this.panelImagen.ResumeLayout(false);
+            this.panelImagen.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPalabraEscondida;
+        private System.Windows.Forms.Label labelPalabraEscondida;
+        private System.Windows.Forms.Panel panelImagen;
+        private System.Windows.Forms.Label labelImage;
+    }
+}
+
